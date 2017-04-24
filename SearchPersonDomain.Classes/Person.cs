0f9909efdad055751f9ee2b2b453b46997ec3550
AdamGoodwin <Suji.kun@gmail.com>
2017-04-24 16:47:05 -0600

    Add files via upload

---
+++ SearchPersonDomain.Classes/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchPersonDomain.Classes
+{
+    public class Person
+    {
+        public Person()
+        {
+            Hobbies = new List<Interests>();
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public Profession Profession { get; set; }
+        public int ProfessionId { get; set; }
+        public virtual List<Interests> Hobbies { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ SearchPersonDomain.Classes/Profession.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchPersonDomain.Classes
+{
+    public class Profession
+    {
+        public Profession()
+        {
+            People = new List<Person>();
+        }
+        public int Id { get; set; }
+        public string ProfessionName { get; set; }
+        public List<Person> People { get; set; }
+    }
+}
